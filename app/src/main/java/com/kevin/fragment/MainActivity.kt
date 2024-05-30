427094653f0123524347ee9270ae8a418d5b4c6e
josedavidp12 <perafandavid322@gmail.com>
2024-05-30 14:25:18 -0500

    fragment 2

--- app/src/main/java/com/kevin/fragment/MainActivity.kt
+++ app/src/main/java/com/kevin/fragment/MainActivity.kt
@@ -8,14 +8,20 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.fragment.app.add
 import androidx.fragment.app.commit
+import androidx.fragment.app.replace
 import com.kevin.fragment.FirstFragment.Companion.ADDRESS_BUNDLE
 import com.kevin.fragment.FirstFragment.Companion.NAME_BUNDLE
+import com.kevin.fragment.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity()  {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+
         if(savedInstanceState == null){
         val bundle = bundleOf(NAME_BUNDLE to "Jose David", ADDRESS_BUNDLE to "Mi casa")
         supportFragmentManager.commit {
@@ -23,5 +29,14 @@ class MainActivity : AppCompatActivity() {
             add<FirstFragment>(R.id.FragmentContainer, args = bundle)
         }
         }
+    binding.Btn1.setOnClickListener{
+        //creo un objeto de tipo fragment
+        val  secondFragment = SecondFragment()
+        supportFragmentManager.beginTransaction().apply {
+            //reemplazo por el second fragment por el fragment container
+            replace(R.id.FragmentContainer, secondFragment)
+            commit()
+        }
+    }
     }
 }
